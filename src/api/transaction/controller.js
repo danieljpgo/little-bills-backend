c37f2c9478fa354c7536bbd86670c763fd15267c
Daniel Jorge <djpgonzaga@gmail.com>
2019-11-27 21:40:02 -0300

    create transication with income or expense values

--- src/api/transaction/controller.js
+++ src/api/transaction/controller.js
@@ -1,12 +1,30 @@
 import { success, notFound } from '../../services/response/'
 import { Category } from '../category'
 import { Transaction } from '.'
+import { Wallet } from '../wallet'
 
-export const create = ({ user, bodymen: { body } }, res, next) =>
-  Transaction.create({ ...body, user })
-    .then((transaction) => transaction.view(true))
-    .then(success(res, 201))
-    .catch(next)
+export const create = ({ user, bodymen: { body } }, res, next) => {
+  console.log(body.category)
+  Category.findById(body.category)
+  .then((category) => {
+    Wallet.findById(body.wallet)
+      .then((wallet) => {
+        if (category.type === "income") {
+          wallet.balance = wallet.balance + body.value;
+          wallet.save();
+        } else {
+          wallet.balance = wallet.balance - body.value;
+           wallet.save();
+        }
+      })
+      .then(() => {
+        Transaction.create({ ...body, user })
+          .then(transaction => transaction.view(true))
+          .then(success(res, 201))
+          .catch(next);
+      });
+  })
+}
 
 export const createIncome = ({ user, bodymen: { body } }, res, next) =>
   Transaction.create({ ...body, user })
